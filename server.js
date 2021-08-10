9a9f407b209cc49ec9eeee2e8c1d4db98bd14fca
GabrielRuiseco19170012 <19170012@uttcampus.edu.mx>
2021-08-10 12:00:00 -0500

    deploy

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/orion-client'));
+app.use(express.static(__dirname + '/dist/orion-client'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/orion-client/'}),
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/myapp/index.html'));
+  }
 );
 
 // Start the app by listening on the default Heroku port
